e4a915b5b5b17167821afa64a86936df53caf0b4
Hayden Lane <hayden@haydendlane.com>
2018-07-12 00:46:27 -0500

    update js and html

--- alabamaCanasca.js
+++ alabamaCanasca.js
@@ -18,7 +18,7 @@ document.getElementById("reset-score").addEventListener("click", function(event)
     
     var confirmReset = confirm("Do you want to reset score?");
     if (confirmReset == true) {
-        scoreBoard.querySelectorAll("p").innerHTML = "";
+        scoreBoard.querySelectorAll("p").innerHTML = "0";
         var teams = [
             {'Name': "team1", 'Points': 0},
             {'Name': "team2", 'Points': 0},
@@ -30,25 +30,6 @@ document.getElementById("reset-score").addEventListener("click", function(event)
 });
 
 // Input number of clean / dirty books & calculate
-document.getElementById("plus-one").addEventListener("click",function(event) {
-    
-    event.preventDefault();
-
-    counter += 1;
-    document.getElementById("clean-books").value = counter;
-});
-document.getElementById("minus-one").addEventListener("click",function(event) {
-    
-    event.preventDefault();
-    
-    if (counter <= 0){
-        counter = 0;
-        document.getElementById("clean-books").value = counter;
-    } else {
-        counter -= 1;
-        document.getElementById("clean-books").value = counter;
-    }
-});
 
 function calculatePoints() {
     document.getElementById("calculate-points").addEventListener("click",function(event) {
@@ -63,34 +44,29 @@ function calculatePoints() {
 }
 
 function addPoints(team) {
-                event.preventDefault();
-                team = team.toString();
-                var index = teams.findIndex(teams => teams.Name === team);
-                var thisForm = document.forms[team]
-                teams[index].Points += ((thisForm.elements.clean.value)*300) + ((thisForm.elements.dirty.value)*100);
-                console.log(teams[index].Points);
-                document.getElementById(team+"-preview").innerHTML = teams[index].Points;
+    event.preventDefault();
+    team = team.toString();
+    var index = teams.findIndex(teams => teams.Name === team);
+    var thisForm = document.forms[team]
+    teams[index].Points += ((thisForm.elements.clean.value)*300) + ((thisForm.elements.dirty.value)*100);
+    console.log(teams[index].Points);
+    document.getElementById(team+"-preview").innerHTML = teams[index].Points;
 }
 function previewPoints(value) {
-                var preview = document.getElementById(value.name+"-preview");
-                var previewPoints = 0;
-                var thisFormInput = value.getElementsByTagName("input");
-                console.log(thisFormInput);
-                for (i=0; i < thisFormInput.length; i++) {
-                                if (thisFormInput[i].value >= 1) {
-                                                switch(thisFormInput[i].name) {
-                                                                case "clean":
-                                                                                previewPoints += thisFormInput[i].value * 300;
-                                                                                break;
-                                                                case "dirty":
-                                                                                previewPoints += thisFormInput[i].value * 100;
-                                                                                break;
-                                                                case "cards":
-                                                                                previewPoints += thisFormInput[i].value * 50;
-                                                }
-                                } else {
-                                                continue
-                                }
-                                preview.innerHTML = previewPoints;
-                }
+    var preview = document.getElementById(value.name+"-preview");
+    var previewPoints = 0;
+    var thisFormInput = value.getElementsByTagName("input");
+    for (i=0; i < thisFormInput.length; i++) {
+        switch(thisFormInput[i].name) {
+            case "clean":
+                previewPoints += thisFormInput[i].value * 300;
+                break;
+            case "dirty":
+                previewPoints += thisFormInput[i].value * 100;
+                break;
+            case "cards":
+                previewPoints += thisFormInput[i].value * 50;
+        }
+        preview.innerHTML = previewPoints;
+    }
 }
