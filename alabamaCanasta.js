4eb5c45986e29a6051a083365885f9e0bf7851dd
Hayden Lane <hayden@haydendlane.com>
2018-08-08 00:08:48 -0500

    Added dictionary for cards, and cleaned up HTML form

--- alabamaCanasta.js
+++ alabamaCanasta.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var teams = [
     {'Name': "team1", 'Points': 0},
     {'Name': "team2", 'Points': 0},
@@ -9,6 +11,13 @@ var counter = 0;
 
 const CLEAN_BOOK = 200;
 const DIRTY_BOOK = 100;
+const CARDS_DICT = {
+    numbered: 5,
+    face: 10,
+    wild: 30,
+    joker: 50,
+    redThree: 300
+};
 
 // Input number of clean / dirty books & calculate
 
@@ -19,11 +28,12 @@ function addPoints(team) {
     var index = teams.findIndex(teams => teams.Name === team);
     var thisForm = document.forms[team + "-add"]
     teams[index].Points += ((thisForm.elements.clean.value)*CLEAN_BOOK) + ((thisForm.elements.dirty.value)*DIRTY_BOOK) + 
-    ((thisForm.elements.cards.value)*10);
+    ((thisForm.elements['cards-numbered'].value)*CARDS_DICT.numbered) + ((thisForm.elements['cards-face'].value)*CARDS_DICT.face) +
+    ((thisForm.elements['cards-wild'].value)*CARDS_DICT.wild) + ((thisForm.elements['cards-joker'].value)*CARDS_DICT.joker);
     document.getElementById(team+"-score").innerHTML = teams[index].Points;
 
     var thisFormInput = thisForm.getElementsByTagName("input");
-    for (i=0; i < thisFormInput.length; i++) {
+    for (let i=0; i < thisFormInput.length; i++) {
         thisFormInput[i].value = 0;
     }
     var preview = document.getElementById(team + "-add-preview");
@@ -36,12 +46,13 @@ function subtractPoints(team) {
     var team = team.toString();
     var index = teams.findIndex(teams => teams.Name === team);
     var thisForm = document.forms[team + "-subtract"]
-    teams[index].Points -= ((thisForm.elements.clean.value)*CLEAN_BOOK) + ((thisForm.elements.dirty.value)*DIRTY_BOOK) + 
-    ((thisForm.elements.cards.value)*10);
+    teams[index].Points -= ((thisForm.elements['cards-numbered'].value)*CARDS_DICT.numbered) + ((thisForm.elements['cards-face'].value)*CARDS_DICT.face) +
+    ((thisForm.elements['cards-wild'].value)*CARDS_DICT.wild) + ((thisForm.elements['cards-joker'].value)*CARDS_DICT.joker) +
+    ((thisForm.elements['cards-red-three'].value)*CARDS_DICT.redThree);
     document.getElementById(team+"-score").innerHTML = teams[index].Points;
 
     var thisFormInput = thisForm.getElementsByTagName("input");
-    for (i=0; i < thisFormInput.length; i++) {
+    for (let i=0; i < thisFormInput.length; i++) {
         thisFormInput[i].value = 0;
     }
     var preview = document.getElementById(team + "-subtract-preview");
@@ -52,7 +63,7 @@ function previewPoints(value) {
     var preview = document.getElementById(value.name + "-preview");
     var previewPoints = 0;
     var thisFormInput = value.getElementsByTagName("input");
-    for (i=0; i < thisFormInput.length; i++) {
+    for (let i=0; i < thisFormInput.length; i++) {
         switch(thisFormInput[i].name) {
             case "clean":
                 previewPoints += thisFormInput[i].value * CLEAN_BOOK;
@@ -60,8 +71,20 @@ function previewPoints(value) {
             case "dirty":
                 previewPoints += thisFormInput[i].value * DIRTY_BOOK;
                 break;
-            case "cards":
-                previewPoints += thisFormInput[i].value * 10;
+            case "cards-numbered":
+                previewPoints += thisFormInput[i].value * CARDS_DICT.numbered;
+                break;
+            case "cards-face":
+                previewPoints += thisFormInput[i].value * CARDS_DICT.face;
+                break;
+            case "cards-wild":
+                previewPoints += thisFormInput[i].value * CARDS_DICT.wild;
+                break;
+            case "cards-joker":
+                previewPoints += thisFormInput[i].value * CARDS_DICT.joker;
+                break;
+            case "cards-red-three":
+                previewPoints += thisFormInput[i].value * CARDS_DICT.redThree;
         }
         preview.innerHTML = previewPoints;
     }
